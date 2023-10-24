de52715228b7999a15b7149f65c2bec743d9600d
IzzatirRofiah <144086912+IzzatirRofiah@users.noreply.github.com>
2023-10-24 14:22:34 +0700

    Add files via upload

---
+++ ArrayNilai16_1.java
@@ -0,0 +1,18 @@
+package Jobsheet9;
+import java.util.Scanner;
+   
+class ArrayNilai16_1 {
+  public static void main(String args[]){  
+    Scanner input16 = new Scanner(System.in);
+
+     int [] nilaiAkhir = new int [10];
+
+    for (int i = 0; i < nilaiAkhir.length; i++) {
+        System.out.print("Masukkan nilai akhir ke-" + i + " : ");
+        nilaiAkhir [i] = input16.nextInt();
+    }
+    for (int i = 0; i< 10; i++) {
+        System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+    }
+  }
+}
