f1f279306242f7d802fd4b1a3b83bc2758535ff9
IzzatirRofiah <144086912+IzzatirRofiah@users.noreply.github.com>
2023-10-24 13:40:14 +0700

    Add files via upload

---
+++ ArrayBilangan16_1.java
@@ -0,0 +1,12 @@
+package Jobsheet9;
+import java.util.Scanner;
+   
+class ArrayBilangan16_1 {
+  public static void main(String args[]){  
+    int[] bil = {5, 13, -7, 17};
+
+    for (int i : bil ){
+        System.out.println(i);
+    }
+  }
+}
---
+++ ArrayBilangan16_2.java
@@ -0,0 +1,12 @@
+package Jobsheet9;
+import java.util.Scanner;
+   
+class ArrayBilangan16_2 {
+  public static void main(String args[]){ 
+    int[] bil = {5, 13, -7, 17};
+
+    for (int i = 0; i <= 4; i++) {
+        System.out.println(bil[i]);
+    }
+  }
+}
